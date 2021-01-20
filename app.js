271bd052adde9c0303bdcc1b03e2e93479a19726
Sidney Arcidiacono <sidneyarci@gmail.com>
2021-01-20 15:32:30 -0800

    Included search form in home template.

--- app.js
+++ app.js
@@ -13,8 +13,8 @@ app.set('view engine', 'handlebars')
 
 // Routes
 app.get('/', (req, res) => {
-  const gifUrl = 'https://media1.tenor.com/images/561c988433b8d71d378c9ccb4b719b6c/tenor.gif?itemid=10058245'
-  res.render('hello-gif', { gifUrl })
+  console.log(req.query) // => "term: hey"
+  res.render('home')
 })
 
 app.get('/greetings/:name', (req, res) => {
