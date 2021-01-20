8ac4c5c694c7b140ff6b04d7697bffe2081a057a
Sidney Arcidiacono <sidneyarci@gmail.com>
2021-01-20 15:28:26 -0800

    Including more middleware to shape out tutorial routes.

--- app.js
+++ app.js
@@ -5,10 +5,21 @@ const express = require('express')
 const app = express()
 
 // Middleware
+const exphbs = require('express-handlebars')
+
+// Tell our app what to parse when we tell it to res.render()
+app.engine('handlebars', exphbs({defaultLayout: 'main'}))
+app.set('view engine', 'handlebars')
 
 // Routes
 app.get('/', (req, res) => {
-  res.send('Hello Squirrel!')
+  const gifUrl = 'https://media1.tenor.com/images/561c988433b8d71d378c9ccb4b719b6c/tenor.gif?itemid=10058245'
+  res.render('hello-gif', { gifUrl })
+})
+
+app.get('/greetings/:name', (req, res) => {
+  const name = req.params.name
+  res.render('greetings', { name })
 })
 
 // Start Server
